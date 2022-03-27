bb2f99dbaeb5a29650514200da1ac7b2768922a0
Redbonelex <alexdurand12@hotmail.com>
2022-03-27 14:14:29 -0700

    Ajout de la recherche d'ancrage et préparation pour comm

--- Code/PiCamera/Object_Detection_Files/ImageProcessing.py
+++ Code/PiCamera/Object_Detection_Files/ImageProcessing.py
@@ -5,14 +5,29 @@ import numpy as np
 from matplotlib import pyplot as plt
 from ZoneInfo import Zone
 
+DIM=(640, 480)
+K=np.array([[256.5223090219522, 0.0, 300.8492878302576], [0.0, 255.95393505302218, 200.83738221162403], [0.0, 0.0, 1.0]])
+D=np.array([[-0.03309970266931218], [-0.035713407140983686], [0.03466182058100104], [-0.014366025635664394]])
+def undistort(img_path):
+    img = cv.imread(img_path)
+    h,w = img.shape[:2]
+    map1, map2 = cv.fisheye.initUndistortRectifyMap(K, D, np.eye(3), K, DIM, cv.CV_16SC2)
+    undistorted_img = cv.remap(img, map1, map2, interpolation=cv.INTER_LINEAR, borderMode=cv.BORDER_CONSTANT)
+    cv.imshow("undistorted", undistorted_img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+    return undistorted_img
+
+
 #Chargement de l'image
 # 1 pour couleur, 0 pour grayscale
-image = cv.imread('LitiereColorer.jpg',1)
+#image = cv.imread('red0.jpg',1)
+image = undistort('red20.jpg')
 #Création d'une image pour pouvoir la modifier
 OG_image = image.copy()
 
 #Application de filtre sur l'image pour facilité la recherche de zones souillées
-image = cv.GaussianBlur(image, (5,5),1)
+#image = cv.GaussianBlur(image, (5,5),1)
 #image = cv.medianBlur(image,5)
 #kernel = np.ones((5,5),np.float32)/25
 #image = cv.filter2D(image,-1,kernel)
@@ -21,8 +36,8 @@ image = cv.GaussianBlur(image, (5,5),1)
 hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
 
 # Définie la couleur à trouvé en HSV
-lower_brown = np.array([10,100,100])
-upper_brown = np.array([40,200,200])
+lower_brown = np.array([255,160,255])
+upper_brown = np.array([255,255,255])
 
 # Créer l'image en binaire sur la couleur rechercher
 mask = cv.inRange(hsv, lower_brown, upper_brown)
