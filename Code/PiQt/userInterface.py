2d13aa9f82bde754b43c91ada74686a54dd5d94c
Habibi501 <moha5160@usherbrooke.ca>
2022-04-09 13:32:01 -0400

    Commentaires et séparation des fichiers

--- Code/PiQt/userInterface.py
+++ Code/PiQt/userInterface.py
@@ -5,51 +5,68 @@ from PyQt5.QtGui     import *
 from PyQt5.QtCore    import *
 from PyQt5           import *
 
-from mainWindow_geometry import Ui_MainWindow
-from popup_geometry      import Ui_Dialog
-from functions           import *
-from init_stop_Cmds      import *
+from mainWindow_geometry           import Ui_MainWindow
+from popup_geometry                import Ui_Dialog
+from Main.functions_Comm           import *
+from Main.init_stop_Sequences      import *
 
+""" Counter of the number of cleaning cycles
+"""
 COMPTEUR = 0
 
+# Class defining the pop-up window object
 class MyDialog(QDialog):
     def __init__(self):
+        """ Initialization of the window
+
+        Call communication between windows function in loop
+        """
         super().__init__()
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
+
+
         self.ui.Fait.clicked.connect(self.change)
 
 
     def change(self):
+        """ Function for transition between pop-up window and main window
+
+        Close pop-up window and open the main one
+        Put the counter variable at 0
+        """
         self.close()
         self.mywindow = MyWindow.__new__(MyWindow)
         self.mywindow.show()
+
         
         global COMPTEUR
         COMPTEUR = 0
         print("Réinistialisation du compteur\n")
 
 
+# Class defining the main window object
 class MyWindow(QMainWindow):
     def __init__(self):
+        """ Initialization of the window
+
+        Initialize the communication between Pi and other composants
+        Initialize the variable in the windows
+        Call communication between windows and componants functions in loop
+        """
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         
-        
-        #Initialisation de la commuication
+
         port, ser = init_sequence()
         
 
-        ##Initialisation de la fenêtre principale
         self.initAnimals()
-        self.commWindows()
+        #self.commWindows()
         self.setActivityHours()
         
 
-
-        
-        #Fonctions de communication
         self.numAnimals()
         self.buttonactiveCage(ser)
         self.buttonclean(ser)
@@ -58,28 +75,44 @@ class MyWindow(QMainWindow):
 
 
     def buttonactiveCage(self, ser):
+        """ Actions tied to the Activation button
+        """
         self.ui.Activation.clicked.connect(lambda:self.activeCage(ser))
 
 
     
     def activeCage(self, ser):
+        """ Get the cage in the activated state
+        """
         activate_state(ser)
 
 
     def buttonstopCage(self, ser):
+        """ Actions tied to the Desactivation button
+        """
         self.ui.Desactivation.clicked.connect(lambda:self.stopCage(ser))
 
     
     def stopCage(self, ser):
+        """ Get the cage in the stop state
+        """
         stop_state(ser)
 
     
     def buttontrash(self, ser):
+        """ Actions tied to the Viderpoubelle button
+        """
         self.ui.Viderpoubelle.clicked.connect(lambda:self.trashCage(ser))
 
 
     def trashCage(self, ser):
+        """ Get the cage in the trash state
+
+        Call function opening the pop-up window
+        """
+        self.dialogbox
         trash_state(ser)
+
 
         """global COMPTEUR
         if  COMPTEUR >= 7: 
@@ -89,10 +122,16 @@ class MyWindow(QMainWindow):
 
 
     def buttonclean(self, ser):
+        """ Actions tied to the Nettoyage button
+        """
         self.ui.Nettoyage.clicked.connect(lambda:self.cleanCage(ser))
 
     
     def cleanCage(self, ser):
+        """ Get the cage in the clean state
+
+        Increment the counter variable and print it
+        """
         clean_state(ser)
 
         global COMPTEUR
@@ -101,11 +140,16 @@ class MyWindow(QMainWindow):
 
 
     def initAnimals(self):
+        """ Initialize the animals check-boxes
+        """
         self.ui.Animal1.setChecked(0)
         self.ui.Animal2.setChecked(0)
         self.ui.Animal3.setChecked(0)
 
+
     def numAnimals(self):
+        """
+        """
         if True :
             self.ui.Animal1.setChecked(0)
         if True :
@@ -114,17 +158,26 @@ class MyWindow(QMainWindow):
             self.ui.Animal3.setChecked(0)
 
 
-    def commWindows(self):
-        self.ui.Viderpoubelle.clicked.connect(self.dialogbox)
+    #def commWindows(self):
+     #   self.ui.Viderpoubelle.clicked.connect(self.dialogbox)
 
 
     def dialogbox(self):
+        """ Function for transition between main window and pop-up window
+
+        Hide the main window and open the pop-up window
+        """
         self.hide()
         self.myDialog = MyDialog()
         self.myDialog.show()
 
 
     def setActivityHours(self):
+        """ Set the activity hours of the cage
+
+        Set starting time
+        Set ending time
+        """
         timeD = QTime()
         timeD.setHMS(8,00,00)
         self.ui.Debut.setTime(timeD)
